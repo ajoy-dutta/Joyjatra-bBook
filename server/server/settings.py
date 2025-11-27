89aa0218469d60b861bbb2b4e58e284501e488a1
fahimfaisal1454 <fahimfaisal1454@gmail.com>
2025-11-27 14:09:08 +0600

    customer & vendor added

--- server/server/settings.py
+++ server/server/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'purchase',
     'stocks',
     'authentication',
+    'people',
 ]
 
 
--- server/server/urls.py
+++ server/server/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,7 @@ urlpatterns = [
     path('api/', include('purchase.urls')),
     path('api/', include('stocks.urls')), 
     path('api/', include('authentication.urls')),
+    path('api/', include('people.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
